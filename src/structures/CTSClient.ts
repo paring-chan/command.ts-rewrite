b5791c73a95765bc7d3ed0f69594c648ed91dfcf
pikokr <parangee9706@gmail.com>
2021-02-09 15:50:32 +0900

    feat: client options

--- src/structures/CTSClient.ts
+++ src/structures/CTSClient.ts
@@ -1,15 +1,20 @@
-import {Client, ClientEvents} from 'discord.js'
+import {Client, ClientEvents, ClientOptions, Message} from 'discord.js'
+import CTSOptions from "../types/CTSOptions";
 
 export default class CTSClient extends Client {
-    constructor() {
-        super()
+    constructor(ctsOpts: CTSOptions, clientOptions?: ClientOptions) {
+        super(clientOptions)
     }
 
-    private _handle<K extends keyof ClientEvents>(event: K, ...args: ClientEvents[K][]) {
+    private _handle<K extends keyof ClientEvents>(event: K, ...args: any[]) {
+        if (event === 'message') return this._handleMsg(args[0])
     }
 
-    emit<K extends keyof ClientEvents>(event: K, ...args): boolean {
-        this._handle(event, ...args)
+    private _handleMsg(msg: Message) {
+    }
+
+    emit(event: string, ...args: any[]): boolean {
+        this._handle(event as keyof ClientEvents, ...args)
         return super.emit(event, ...args);
     }
 }
---
+++ src/types/CTSOptions.ts
@@ -0,0 +1,5 @@
+type CTSOptions = {
+    prefix: string
+}
+
+export default CTSOptions
