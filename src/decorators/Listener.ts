8d33fb9eff3cb5b3fd3576979243d13694d58d89
pikokr <parangee9706@gmail.com>
2021-02-10 19:43:43 +0900

    feat: listener decorator

---
+++ src/decorators/Listener.ts
@@ -0,0 +1,13 @@
+import Module from "../structures/Module";
+
+export function Command(event: string): any {
+    return async (target: any, key: string, descriptor: PropertyDescriptor) => {
+        if (!(target instanceof Module)) throw new Error('Command decorator must be used in `Module` class.')
+        const c = target.constructor as typeof Module
+        c.listeners.push({
+            event,
+            execute: descriptor.value
+        })
+    }
+}
+
